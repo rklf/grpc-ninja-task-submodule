fba22cc99b45a687159ff9027c01f28c36fc2b18
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-02-17 12:30:03 +0100

    feat(stubs): using manually generated stubs

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,16 +1,16 @@
 import { Controller } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { GrpcMethod } from '@nestjs/microservices';
-import taskStub from '@buf/bneiconseil_iw4-ninja.grpc_node/task/v1alpha/task_pb';
+import { CreateTaskRequest } from 'src/stubs/task/v1alpha/task';
 
 @Controller()
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @GrpcMethod('TaskService')
-  createTask(data: taskStub.CreateTaskRequest) {
-    const newTask = data.Task;
+  createTask(data: CreateTaskRequest) {
+    const newTask = data.task;
 
-    return this.taskService.create(newTask);
+    return this.taskService.create(newTask as any);
   }
 }
